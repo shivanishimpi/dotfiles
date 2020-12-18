58f44cb69d64515cc8f849bc7b72dd548def6408
Shivani Shimpi <shivanishimpi9@gmail.com>
2020-12-18 23:45:42 +0530

    ☕️ added-brew-packages

--- setup-cmds.sh
+++ setup-cmds.sh
@@ -1,9 +1,17 @@
-
-echo Installing Homebrew
+#bin/
+echo "Installing Homebrew"
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-echo Installing Oh-My-Zsh
+echo "Installing Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo Running the command to create the locate database at (/var/db/locate.database)
+echo "Running the command to create the locate database at (/var/db/locate.database)"
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+sudo /usr/libexec/locate.updatedb
+
+echo "Installing all homebrew packages"
+while read p; do
+        echo "Installing $p"
+        brew install "$p"
+        echo "Done installing $p"
+done <brew-packages.txt
